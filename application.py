0806f39a41246c9fb5cf05a2fa17c220e64c6ced
abhiyanbeta <abhiyanbeta@rocketmail.com>
2020-09-22 01:27:32 +0100

    Connects SQL database
    Added SQL database and works retrieving data from table
    as per /sqltable.
    Renders table with data from sample SQL table in database.
    
    Also created cards using bootsrap to display the same info.
    Thinking of pivoting towards creating a notes app, rather than
    one for flashcards so as to allow longer form content, kind of
    like a blog post manager but for notes.
    
    With it's special feature being searching through notes.

--- application.py
+++ application.py
@@ -1,7 +1,8 @@
 # Deploy to Heroku: https://www.geeksforgeeks.org/deploy-python-flask-app-on-heroku/
 
-
 import os
+import psycopg2 as SQL  # To connect to the database
+
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -10,7 +11,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 app = Flask(__name__)
 
 # Configure database
-db = SQL("postgres://xxdbvyyiutrzfh:5f34c27570b453c1bf7878ec081efdd290c5cefff7684fd75e9d55c946d91273@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d1r5sk71q9ge8f")
+db = SQL.connect("postgres://xxdbvyyiutrzfh:5f34c27570b453c1bf7878ec081efdd290c5cefff7684fd75e9d55c946d91273@ec2-54-247-94-127.eu-west-1.compute.amazonaws.com:5432/d1r5sk71q9ge8f")
+cur = db.cursor()
 
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -34,6 +36,25 @@ def after_request(response):
 TODO
 '''
 
+# Index page
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+
+# Testing SQL database
+@app.route("/sqltable")
+def sqltable():
+	# SQL query
+	users = cur.execute("SELECT * FROM accounts;")
+	users2 = cur.fetchall()
+	for user in users2:
+		print(user)
+	# Return template, passing in result of SQL query
+	return render_template("sqltable.html", users2=users2)
+
+
+# Add new cards
+@app.route("/add")  #Need to do POST method for submitting form
+def add():
+	return render_template("add.html")
